a1bbffb3890ba47e14d716e94746cb43ee1ada64
dani-furtado <danicfurtado@outlook.com>
2024-05-29 12:55:09 -0300

    Deploy do Projeto Projeto Integrador

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import FormPostagem from './components/postagem/formpostagem/FormPostagem'
 import { ToastContainer } from 'react-toastify'
 
 import 'react-toastify/dist/ReactToastify.css'
+import Perfil from './pages/perfil/Perfil'
 
 function App() {
 
@@ -40,6 +41,7 @@ function App() {
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />
               <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
 
           </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -30,8 +30,8 @@ function Navbar() {
                         <Link to='/postagens' className='hover:underline'>Postagens</Link>
                         <Link to='/temas' className='hover:underline'>Temas</Link>
                         <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
-                        <Link to='/login' className='hover:underline'>Login</Link>                                   
-                        <div className='hover:underline'>Perfil</div>
+                        <Link to='/login' className='hover:underline'>Login</Link>
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>                                   
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
